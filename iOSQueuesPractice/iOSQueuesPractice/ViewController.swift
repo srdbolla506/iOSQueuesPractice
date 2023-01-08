086b823cddb9c088b6e2aa372b8fdee93c72a77b
Sri Divya Bolla <sridivya.bolla@gmail.com>
2023-01-08 19:57:51 +0530

    Creation of Xcode project

---
+++ iOSQueuesPractice/iOSQueuesPractice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iOSQueuesPractice
+//
+//  Created by Sri Divya Bolla on 08/01/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
